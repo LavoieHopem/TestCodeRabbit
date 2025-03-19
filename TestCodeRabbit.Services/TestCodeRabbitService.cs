fa663ae178bc37762a198f2385c31f256164a169
Mathieu Lavoie <mlavoie@hopem.com>
2025-03-19 13:20:07 -0400

    Test Modification code

---
+++ TestCodeRabbit.Services/TestCodeRabbitService.cs
@@ -0,0 +1,22 @@
+﻿using TestCodeRabbit.Models;
+
+namespace TestCodeRabbit.Services
+{
+    public class TestCodeRabbitService : ITestCodeRabbitService
+    {
+        public RabbitDto GetRabbit()
+        {
+            return new RabbitDto
+            {
+                CreatedDate = DateTime.Now,
+                CountElement = 2,
+                Name = "Test en prod"
+            };
+        }
+
+        public IList<RabbitDto> GetRabbits()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestCodeRabbit/Controllers/CodeRabbitController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestCodeRabbit.Services;
+
+namespace TestCodeRabbit.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CodeRabbitController : ControllerBase
+    {
+        private ITestCodeRabbitService TEST_CODE_RABBIT;
+        public CodeRabbitController(ITestCodeRabbitService testCodeRabbitService)
+        {
+            TEST_CODE_RABBIT = testCodeRabbitService;
+        }
+
+        [HttpGet(Name = "GetRabit")]
+        public IActionResult Get()
+        {
+            try
+            {
+                var rabbit = TEST_CODE_RABBIT.GetRabbit();
+                return Ok(rabbit);
+            }
+            catch (Exception)
+            {
+
+            }
+            return null;
+        }
+    }
+}
